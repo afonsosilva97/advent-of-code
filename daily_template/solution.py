564cd1709696d2ca9ef3a493c89f6fe53897eb18
Afonso Silva <afonso.silva@triptease.com>
2025-12-02 08:58:36 +0000

    Daily template folder

---
+++ daily_template/solution.py
@@ -0,0 +1,16 @@
+def get_answer_part_1():
+    raise NotImplementedError
+
+
+def get_answer_part_2():
+    raise NotImplementedError
+
+
+def run(input_lines, test_name):
+    print(f"\n{test_name}")
+
+    answer_part_1 = get_answer_part_1()
+    print(f"answer_part_1: {answer_part_1}")
+
+    answer_part_2 = get_answer_part_2()
+    print(f"answer_part_2: {answer_part_2}")
